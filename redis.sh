3b01d0f6e4f4299611b7c47e72c71f78cbb434dd
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-15 00:21:03 +0530

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -1,1 +1,6 @@
-
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum module enable redis:remi-6.2 -y
+yum install redis -y 
+#Update listen address from 127.0.0.1 to 0.0.0.0
+systemctl enable redis 
+systemctl start redis 
