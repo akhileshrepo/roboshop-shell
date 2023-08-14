533f95d47900e7d0c7ccf2c5369ad46b27317552
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-15 00:09:54 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,1 +1,5 @@
-
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+yum install mongodb-org -y 
+#update the listen address from 127.0.0.1 to 0.0.0.0 
+systemctl enable mongod 
+systemctl start mongod 
