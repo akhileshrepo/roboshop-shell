3ed9fca1ce39ae246d1be90539de2ee00b3c2ef2
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-15 00:39:42 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,7 @@
+cp mysql.repo /etc/yum.repos.d/mysql.repo
+yum module disable mysql -y
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl restart mysqld  
 
+mysql_secure_installation --set-root-pass RoboShop@1
