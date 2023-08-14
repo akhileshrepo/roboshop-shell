6bb04b1b2697d98b7294eba38479046c5c22a88a
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-15 00:12:51 +0530

    Update catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,1 +1,13 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+cd /app 
+npm install 
 
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
