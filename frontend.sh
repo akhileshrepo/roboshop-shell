ae44ed0cacf2a83c9f48dc3956eef99f5607606b
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-14 23:56:19 +0530

    Create frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,8 @@
+yum install nginx -y 
+systemctl enable nginx 
+systemctl restart nginx
+
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
