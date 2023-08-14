38d721cbb62c85df091e73c9a892ad89ff1c9690
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-15 00:23:48 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,1 +1,18 @@
+cp user.service /etc/systemd/system/user.service
+cp user.repo /etc/yum.repos.d/user.repo
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
+cd /app 
+unzip /tmp/user.zip
+cd /app 
+npm install 
 
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+
+systemctl daemon-reload
+systemctl enable user 
+systemctl restart user
