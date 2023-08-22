481f30a3110cd0d090e329648532973f19df1811
AKhilesh <kothapallyakhil123@gmail.com>
2023-08-23 01:30:24 +0530

    chnages

--- catalogue.sh
+++ catalogue.sh
@@ -14,7 +14,7 @@ npm install
 
 
 yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
 
 systemctl daemon-reload
 systemctl enable catalogue 
