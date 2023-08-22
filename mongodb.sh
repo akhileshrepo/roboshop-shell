57d1b649f0079c1d2661ea7129c8e03dcd368c58
Akhilesh <131031617+akhileshrepo@users.noreply.github.com>
2023-08-22 11:42:26 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org -y 
-#sed -i 's/127.0.0.1/0.0.0.0/' etc/mongodb.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod 
 systemctl start mongod 
