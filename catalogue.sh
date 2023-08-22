741580e5120855ba1a2777bfe60cc8f503a5bfcc
AKhilesh <kothapallyakhil123@gmail.com>
2023-08-22 09:28:29 +0530

    commited chnages

--- catalogue.sh
+++ catalogue.sh
@@ -3,16 +3,17 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-mkdir /app 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install 
+#mkdir /app 
+#curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+#cd /app 
+#unzip /tmp/catalogue.zip
+#cd /app 
+#npm install 
 
-yum install mongodb-org-shell -y
-mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
+#yum install mongodb-org-shell -y
+#mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl restart catalogue
+#systemctl daemon-reload
+#systemctl enable catalogue 
+#systemctl restart catalogue
+*/
