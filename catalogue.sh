77bcd046ee0171f2ab6682708c5147e268e544bf
AKhilesh <kothapallyakhil123@gmail.com>
2023-08-23 01:19:08 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,21 @@
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-rm -rf /app
+rm -rf /app/*
 mkdir /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-
 cd /app 
 unzip /tmp/catalogue.zip
 cd /app 
 npm install 
 
+
 yum install mongodb-org-shell -y
-mongo --host mongodb.akhildevops.online </app/schema/catalogue.js
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
 
 systemctl daemon-reload
 systemctl enable catalogue 
-systemctl restart catalogue
-
+systemctl start catalogue
--- frontend.sh
+++ frontend.sh
@@ -5,5 +5,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+
 systemctl enable nginx 
-systemctl restart nginx
+systemctl restart nginx 
