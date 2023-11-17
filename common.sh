30eb6c8ec031441df4282f2ddb70301beb845fd4
AKhilesh <kothapallyakhil123@gmail.com>
2023-11-17 19:31:34 +0530

    updated code

--- common.sh
+++ common.sh
@@ -53,7 +53,7 @@ func_schema_setup() {
     func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-    mongo --host mongodb.rdevopsb72.online </app/schema/${component}.js &>>${log}
+    mongo --host 172.31.25.109 </app/schema/${component}.js &>>${log}
     func_exit_status
   fi
 
