40e4ff5d76edf34d49afa871e6c4dd4a0d475769
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-22 12:02:55 +0530

    updated code

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,25 @@
-component=catalogue
-schema_type=mongodb
-source common.sh
+#component=catalogue
+#schema_type=mongodb
+#source common.sh
 
-func_nodejs
+#func_nodejs
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
