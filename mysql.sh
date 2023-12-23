30cf2eb484a5b1022e599a10ec525a6bf9451bee
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-23 15:24:47 +0530

    updated code

--- mysql.sh
+++ mysql.sh
@@ -11,9 +11,10 @@
 #systemctl start mysqld
 #mysql_secure_installation --set-root-pass ${mysql_root_password}
 
+
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 yum module disable mysql -y
 yum install mysql-community-server -y
 systemctl enable mysqld
-systemctl restart mysqld
+systemctl start mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
