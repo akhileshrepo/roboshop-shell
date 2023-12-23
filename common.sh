7d41cb2aa666da40f383eee2eb7ffe1c00b7b6c7
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-23 18:48:53 +0530

    updated code

--- common.sh
+++ common.sh
@@ -71,9 +71,9 @@
 #}
 func_exit_status() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31mSUCCESS\e[0m"
+    echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo -e "\e[32mFAILURE\e[0m"
+    echo -e "\e[31mFAILURE\e[0m"
   fi
 }
 
