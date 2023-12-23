1615236e76138ab39a3b79beecf5334a5cea24a6
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-23 15:08:04 +0530

    updated code

--- common.sh
+++ common.sh
@@ -108,7 +108,7 @@ func_systemd() {
 func_schema_setup() {
     log=/tmp/roboshop.log
 
-    if [ "${schema_type}" == "mongodb"]; then
+    if [ "${schema_type}" == "mongodb" ]; then
       echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install Mongo client<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
       yum install mongodb-org-shell -y    &>>${log}
 
@@ -116,12 +116,12 @@ func_schema_setup() {
       mongo --host mongodb.akhildevops.online </app/schema/${component}.js   &>>${log}
     fi
 
-    if [ "${schema_type}" == "mysql"]; then
+    if [ "${schema_type}" == "mysql" ]; then
       echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>> Install mysql client <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-        dnf install mysql -y  &>>${log}
+      dnf install mysql -y  &>>${log}
 
-        echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>> Load schema <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-        mysql -h mysql.akhildevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql  &>>${log}
+      echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>> Load schema <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+      mysql -h mysql.akhildevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql  &>>${log}
     fi
 }
 
