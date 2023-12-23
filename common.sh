751fc7b5bb69bea1cc8cb364cb0923e98dede336
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-23 13:18:56 +0530

    updated code

--- common.sh
+++ common.sh
@@ -120,3 +120,22 @@ func_nodejs() {
   systemctl restart ${component}   &>>${log}
 }
 
+func_java() {
+  cp shipping.service /etc/systemd/system/shipping.service
+  yum install maven -y
+  useradd roboshop
+  mkdir /app
+  curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+  cd /app
+  unzip /tmp/shipping.zip
+  cd /app
+  mvn clean package
+  mv target/shipping-1.0.jar shipping.jar
+
+  dnf install mysql -y
+  mysql -h mysql.akhildevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+  systemctl daemon-reload
+  systemctl enable shipping
+  systemctl restart shipping
+}
--- shipping.sh
+++ shipping.sh
@@ -1,23 +1,6 @@
-#component=shipping
+component=shipping
 #schema_type=mysql
-#source common.sh
+source common.sh
 
-#func_java
+func_java
 
-cp shipping.service /etc/systemd/system/shipping.service
-yum install maven -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-dnf install mysql -y
-mysql -h mysql.akhildevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
