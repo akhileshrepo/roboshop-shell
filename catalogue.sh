d542f9adb7234c719a76ec9a8084d51832382864
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-23 12:11:26 +0530

    updated code

--- catalogue.sh
+++ catalogue.sh
@@ -5,45 +5,45 @@
 #func_nodejs
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>> Create Catalogue service <<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>  create Mongodb repos  <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>  Install Nodejs Repos  <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>> Install Nodejs <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-yum install nodejs -y   &>/tmp/roboshop.log
+yum install nodejs -y   &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-useradd roboshop  &>/tmp/roboshop.log
+useradd roboshop  &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> remove the old content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-rm -rf /app  &>/tmp/roboshop.log
+rm -rf /app  &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mkdir /app &>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cd /app  &>/tmp/roboshop.log
-unzip /tmp/catalogue.zip   &>/tmp/roboshop.log
-cd /app   &>/tmp/roboshop.log
+cd /app  &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip   &>>/tmp/roboshop.log
+cd /app   &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install Nodejs dependencies <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-npm install   &>/tmp/roboshop.log
+npm install   &>>/tmp/roboshop.log
 
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install Mongo client<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y    &>/tmp/roboshop.log
+yum install mongodb-org-shell -y    &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Load catalogue schema <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mongo --host mongodb.akhildevops.online </app/schema/catalogue.js   &>/tmp/roboshop.log
+mongo --host mongodb.akhildevops.online </app/schema/catalogue.js   &>>/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Start catalogue service <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload   &>/tmp/roboshop.log
-systemctl enable catalogue   &>/tmp/roboshop.log
-systemctl restart catalogue   &>/tmp/roboshop.log
+systemctl daemon-reload   &>>/tmp/roboshop.log
+systemctl enable catalogue   &>>/tmp/roboshop.log
+systemctl restart catalogue   &>>/tmp/roboshop.log
