8ef5146c070da941cc939ea5ef10a29a672ff582
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-23 12:17:52 +0530

    updated code

--- catalogue.sh
+++ catalogue.sh
@@ -10,33 +10,33 @@ echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>> Create Catalogue service <<<<<<<<<<<
 cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>>  create Mongodb repos  <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log &>>${log}
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>>>  Install Nodejs Repos  <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>> Install Nodejs <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-yum install nodejs -y   &>>/tmp/roboshop.log &>>${log}
+yum install nodejs -y    &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-useradd roboshop  &>>/tmp/roboshop.log &>>${log}
+useradd roboshop   &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> remove the old content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-rm -rf /app  &>>/tmp/roboshop.log &>>${log}
+rm -rf /app   &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mkdir /app &>>/tmp/roboshop.log  &>>${log}
+mkdir /app   &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cd /app  &>>/tmp/roboshop.log   &>>${log}
-unzip /tmp/catalogue.zip   &>>/tmp/roboshop.log  &>>${log}
-cd /app   &>>/tmp/roboshop.log  &>>${log}
+cd /app  &>>${log}
+unzip /tmp/catalogue.zip     &>>${log}
+cd /app    &>>${log}
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install Nodejs dependencies <<<<<<<<<<<<<<<<<<<<<<\e[0m"
-npm install   &>>/tmp/roboshop.log  &>>${log}
+npm install    &>>${log}
 
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install Mongo client<<<<<<<<<<<<<<<<<<<<<<\e[0m"
