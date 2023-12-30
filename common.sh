1b56fe91b00c3112b324e4d5a5d76dd0a2d19191
AKhilesh <kothapallyakhil123@gmail.com>
2023-12-30 19:47:46 +0530

    updated code

--- common.sh
+++ common.sh
@@ -82,7 +82,7 @@ func_nodejs() {
  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS  <<<<<<<<<<<<\e[0m"
-  dnf install nodejs -y &>>${log}
+  dnf install nodejs -y
   func_exit_status
 
   func_apppreq
