50feb0cd9fe426534516f4e5f7098d10b7734e55
AKhilesh <kothapallyakhil123@gmail.com>
2024-06-14 03:16:16 +0530

    updated DNS records

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,18 @@
+source common.sh
+
+echo -e "\e[36m>>>>>>>>>>> Download mongo repos <<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>> Install Mongodb <<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org -y
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>> update listen address <<<<<<<<<<<<<<\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<\e[0m"
 systemctl enable mongod
 systemctl restart mongod
+func_exit_status
